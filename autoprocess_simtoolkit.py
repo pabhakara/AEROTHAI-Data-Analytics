65611a50728c7256e4b9409d25d59aa31755b792
pabhakara <84214769+pabhakara@users.noreply.github.com>
2021-11-24 07:26:52 +0700

    test2

--- autoprocess_simtoolkit.py
+++ autoprocess_simtoolkit.py
@@ -12,12 +12,11 @@ def toc():
         print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
     else:
         print("Toc: start time not set")
-
 tic()
 
-from dbname_and_paths import db_name
+from dbname_and_paths import db_name,path_script
 
-path_script = "/Users/pongabha/Dropbox/Workspace/PycharmProjects/AEROTHAI_Data_Analytics/"
+#path_script = "/Users/pongabha/Dropbox/Workspace/PycharmProjects/AEROTHAI_Data_Analytics/"
 #path_db = '/Users/pongabha/Dropbox/Workspace/AEROTHAI Data Analytics/NavData/simtoolkitpro_native_2111/'
 #
 # # #establishing the connection
@@ -34,14 +33,13 @@ path_script = "/Users/pongabha/Dropbox/Workspace/PycharmProjects/AEROTHAI_Data_A
 # print("Database created successfully........")
 # conn.close()
 
-#Populating the database
+#Populating the database with simtoolkit navdata from sqlite file
 exec(open(path_script + 'SQLite_File_to_PostgreSQL.py').read())
 
 # #establishing the connection
 conn2 = psycopg2.connect(
    database=db_name, user='postgres', password='password', host='127.0.0.1', port='5432'
 )
-
 conn2.autocommit = True
 cursor2 = conn2.cursor()
 sql_file = open(path_script + 'create_wp.sql', 'r')
@@ -49,16 +47,16 @@ cursor2.execute(sql_file.read())
 conn2.close()
 # # #
 
-# exec(open(path_script + 'Create_MORA_Grid_simtoolkit.py').read())
-# exec(open(path_script + 'Create_SID_Legs_simtoolkit.py').read())
-# exec(open(path_script + 'Create_STAR_Legs_simtoolkit.py').read())
-# exec(open(path_script + 'Create_IAP_Legs_RF_simtoolkit.py').read())
-# exec(open(path_script + 'Create_IAP_Legs_AF_simtoolkit.py').read())
-# exec(open(path_script + 'Create_IAP_Legs_simtoolkit.py').read())
-# exec(open(path_script + 'Create_ATS_Route_Segments_simtoolkit.py').read())
-# exec(open(path_script + 'Create_ATS_Route_simtoolkit.py').read())
-# exec(open(path_script + 'Create_Runway_Segments_simtoolkit.py').read())
-# exec(open(path_script + 'Create_Holding_Legs.py').read())
+exec(open(path_script + 'Create_MORA_Grid_simtoolkit.py').read())
+exec(open(path_script + 'Create_SID_Legs_simtoolkit.py').read())
+exec(open(path_script + 'Create_STAR_Legs_simtoolkit.py').read())
+exec(open(path_script + 'Create_IAP_Legs_RF_simtoolkit.py').read())
+exec(open(path_script + 'Create_IAP_Legs_AF_simtoolkit.py').read())
+exec(open(path_script + 'Create_IAP_Legs_simtoolkit.py').read())
+exec(open(path_script + 'Create_ATS_Route_Segments_simtoolkit.py').read())
+exec(open(path_script + 'Create_ATS_Route_simtoolkit.py').read())
+exec(open(path_script + 'Create_Runway_Segments_simtoolkit.py').read())
+exec(open(path_script + 'Create_Holding_Legs.py').read())
 exec(open(path_script + 'Create_Holding_Legs_from_IAPs.py').read())
 
 #establishing the connection
