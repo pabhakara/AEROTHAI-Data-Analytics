b31edf2772307b7d39846af50fe8ad4685babb90
pabhakara <84214769+pabhakara@users.noreply.github.com>
2022-03-29 18:28:03 +0700

    test

--- dbname_and_paths.py
+++ dbname_and_paths.py
@@ -1,8 +1,8 @@
 #db_name = 'current_airac'
-db_name = 'airac_2111'
+db_name = 'airac_2110'
 
 path_script = "/Users/pongabha/Dropbox/Workspace/PycharmProjects/AEROTHAI_Data_Analytics/"
-airac = '_2111'
+airac = '_2110'
 path_db = '/Users/pongabha/Dropbox/Workspace/AEROTHAI Data Analytics/NavData/simtoolkitpro_native' + airac +'/'
 
 # set airac to blank when populating current_airac
