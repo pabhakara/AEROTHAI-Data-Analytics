955d9268a62d401ab809366c83cb5dfc919c1cb8
pabhakara <84214769+pabhakara@users.noreply.github.com>
2022-07-19 20:16:28 +0700

    test

---
+++ Equipage_Analysis.py
@@ -0,0 +1,77 @@
+import psycopg2.extras
+import pandas as pd
+import plotly.express as px
+pd.options.plotting.backend = "plotly"
+
+schema_name = 'flight_data'
+conn_postgres = psycopg2.connect(user = "pongabhaab",
+                                  password = "pongabhaab",
+                                  host = "172.16.129.241",
+                                  port = "5432",
+                                  database = "aerothai_dwh",
+                                  options="-c search_path=dbo," + schema_name)
+year_list = ['2021','2020','2019','2018','2017','2016','2015','2014','2013']
+#year_list = ['2022']
+month_list = ['01','02','03','04','05','06','07','08','09','10','11','12']
+#month_list = ['01','02','03','04','05','06']
+filter = {
+    "Mode-S aircraft identification":"(item10_cns like '%/%S%' "
+                                     "or item10_cns like '%/%L%' "
+                                     "or item10_cns like '%/%E%' "
+                                     "or item10_cns like '%/%H%' "
+                                     "or item10_cns like '%/%I%') ",
+    "Mode-S pressure-altitude": "(item10_cns like '%/%L%' "
+                                "or item10_cns like '%/%E%' "
+                                "or item10_cns like '%/%S%' "
+                                "or item10_cns like '%/%H%'"
+                                "or item10_cns like '%/%P%') ",
+    "Mode-S extended squitter": "(item10_cns like '%/%L%' or item10_cns like '%/%E%' ) ",
+    "Mode-S enhanced surveillance": "(item10_cns like '%/%L%' or item10_cns like '%/%H%' ) ",
+    "No Mode-S": "NOT(item10_cns like '%/%S%' or item10_cns like '%/%L%' or item10_cns like '%/%E%' or item10_cns like '%/%H%'or item10_cns like '%/%I%') ",
+}
+
+# filter = {
+#     "ADS-B":"(item10_cns like '%/%B%'or item10_cns like '%/%U%' or item10_cns like '%/%V%') ",
+# }
+
+equipage_list = filter.keys()
+#equipage_count_df = pd.DataFrame()
+with conn_postgres:
+    for equipage in equipage_list:
+        equipage_count_df = pd.DataFrame()
+        for year in year_list:
+            for month in month_list:
+                print(f"{year}_{month}")
+                #postgres_sql_text = f"SELECT '{year}_{month}','{equipage}',dest,count(*) " \
+                postgres_sql_text = f"SELECT '{year}_{month}','{equipage}',count(*) " \
+                                    f"FROM {schema_name}.\"{year}_{month}_fdmc\" "\
+                                    f"WHERE {filter[equipage]} " \
+                                    f"and dest like 'VT%'" \
+                                    f"and frule like 'I';" \
+                                    #f"GROUP BY dest;"
+                cursor_postgres = conn_postgres.cursor()
+                cursor_postgres.execute(postgres_sql_text)
+                record = cursor_postgres.fetchall()
+                #equipage_count_temp = pd.DataFrame(record, columns=['year_month', 'equipage', 'dest', 'count'])
+                equipage_count_temp = pd.DataFrame(record, columns=['year_month', 'equipage', 'count'])
+                equipage_count_df = (pd.concat([equipage_count_df, equipage_count_temp],ignore_index=True))
+
+                # #postgres_sql_text = f"SELECT '{year}_{month}','No_{equipage}',dest,count(*) " \
+                # postgres_sql_text = f"SELECT '{year}_{month}','No_{equipage}',count(*) " \
+                #                     f"FROM {schema_name}.\"{year}_{month}_fdmc\" "\
+                #                     f"WHERE NOT({filter[equipage]}) " \
+                #                     f"and dest like 'VT%'" \
+                #                     f"and frule like 'I';" \
+                #                     #f"GROUP BY dest;"
+                # cursor_postgres = conn_postgres.cursor()
+                # cursor_postgres.execute(postgres_sql_text)
+                # record = cursor_postgres.fetchall()
+                # #equipage_count_temp = pd.DataFrame(record, columns=['year_month', 'equipage', 'dest', 'count'])
+                # equipage_count_temp = pd.DataFrame(record, columns=['year_month', 'equipage', 'count'])
+                # equipage_count_df = (pd.concat([equipage_count_df, equipage_count_temp],ignore_index=True))
+
+        print(equipage_count_df)
+        fig = px.bar(equipage_count_df.sort_values(['year_month', 'count'],
+                                                   ascending=[True, False]),
+                     x='year_month', y='count', color='equipage')
+        fig.show()
