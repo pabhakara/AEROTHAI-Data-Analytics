d86d16964b6bb420332d7c527fa9e0fd130a061f
pabhakara <84214769+pabhakara@users.noreply.github.com>
2022-08-19 09:51:04 +0700

    test

--- Create_LOS_5sec.py
+++ Create_LOS_5sec.py
@@ -27,7 +27,9 @@ with conn_postgres:
     #month_list = ['04']
     month_list = ['05']
     #day_list = ['24','25','26','27','28','29','30']
-    day_list = ['01','02','03','04','05','06']
+    day_list = ['01','02','03','04','05','06','07','08','09','10',
+                '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+                '21', '22', '23', '24', '25', '26', '27', '28', '29', '30','31']
     for year in year_list:
         for month in month_list:
             for day in day_list:
@@ -38,10 +40,10 @@ with conn_postgres:
                                     f"a.flight_key as flight_key_a, b.flight_key as flight_key_b, " \
                                     f"a.flight_id as flight_id_a, b.flight_id as flight_id_b, " \
                                     f"public.ST_Distance( " \
-                                    f"public.ST_Transform(a.\"2d_position\",3857), " \
-                                    "public.ST_Transform(b.\"2d_position\",3857))/1852 as horizontal_separation, " \
+                                    f"public.ST_Transform(a.\"position\",3857), " \
+                                    "public.ST_Transform(b.\"position\",3857))/1852 as horizontal_separation, " \
                                     "abs(a.measured_fl - b.measured_fl) as vertical_separation, " \
-                                    "public.ST_MakeLine(a.\"2d_position\",b.\"2d_position\") as \"geom\", " \
+                                    "public.ST_MakeLine(a.\"position\",b.\"position\") as \"geom\", " \
                                     "a.measured_fl as fl_a, b.measured_fl as fl_b, " \
                                     "a.ground_speed as ground_speed_a, b.ground_speed as ground_speed_b, " \
                                     "a.vx as vx_a, a.vy as vy_a, " \
@@ -54,8 +56,8 @@ with conn_postgres:
                                     "a.app_time = b.app_time " \
                                     "AND abs(a.measured_fl - b.measured_fl) < 10  " \
                                     "AND (a.measured_fl > 1 AND b.measured_fl > 10)  " \
-                                    "AND public.ST_Distance(public.ST_Transform(a.\"2d_position\",3857), " \
-                                    "public.ST_Transform(b.\"2d_position\",3857))/1852 < 8 " \
+                                    "AND public.ST_Distance(public.ST_Transform(a.\"position\",3857), " \
+                                    "public.ST_Transform(b.\"position\",3857))/1852 < 8 " \
                                     "ORDER BY flight_key_a,flight_key_b,app_time; " \
                                     f"DROP TABLE IF EXISTS los.los_{year}_{month}_{day}_temp; " \
                                     f"SELECT los.los_{year}_{month}_{day}.*, " \
